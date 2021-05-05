60139029c2ec5c834666e9943209ffecba87a8a2
Patrick Prell <pat@Mechatron.localdomain>
2021-05-05 17:00:23 +1000

    add git_max_size cli arg

--- src/main.rs
+++ src/main.rs
@@ -13,138 +13,139 @@ use crate::theme::Theme;
 
 #[derive(Clone, Copy, Eq, PartialEq)]
 pub enum Shell {
-    Bare,
-    Bash,
-    Zsh
+	Bare,
+	Bash,
+	Zsh
 }
 
 pub struct Powerline {
-    segments: Vec<Segment>,
-    theme: Theme,
-    shell: Shell,
+	segments: Vec<Segment>,
+	theme: Theme,
+	shell: Shell,
 
-    #[cfg(feature = "git2")]
-    git: Option<git2::Repository>,
-    #[cfg(feature = "git2")]
-    git_statuses: Option<Vec<git2::Status>>
+	#[cfg(feature = "git2")]
+	git: Option<git2::Repository>,
+	#[cfg(feature = "git2")]
+	git_statuses: Option<Vec<git2::Status>>
 }
 
 fn main() {
-    #[cfg(feature = "flame")]
-    flame::start("clap-rs");
+	#[cfg(feature = "flame")]
+	flame::start("clap-rs");
 
-    let matches = cli::build_cli().get_matches();
+	let matches = cli::build_cli().get_matches();
 
-    #[cfg(feature = "flame")]
-    flame::end("clap-rs");
+	#[cfg(feature = "flame")]
+	flame::end("clap-rs");
 
-    #[cfg(feature = "flame")]
-    flame::start("parse arguments");
+	#[cfg(feature = "flame")]
+	flame::start("parse arguments");
 
-    let cwd_max_depth    = value_t_or_exit!(matches, "cwd-max-depth", u8);
-    let cwd_max_dir_size = value_t_or_exit!(matches, "cwd-max-dir-size", u8);
-    let error            = value_t_or_exit!(matches, "error", u8);
+	let cwd_max_depth		= value_t_or_exit!(matches, "cwd-max-depth", u8);
+	let cwd_max_dir_size	= value_t_or_exit!(matches, "cwd-max-dir-size", u8);
+	let git_max_size		= value_t_or_exit!(matches, "git-max-size", u8);
+	let error				= value_t_or_exit!(matches, "error", u8);
 
-    #[cfg(feature = "flame")]
-    flame::start("parse theme");
+	#[cfg(feature = "flame")]
+	flame::start("parse theme");
 
-    let theme = if let Some(file) = matches.value_of("theme") {
-        if let Ok(theme) = theme::load(file) {
-            theme
-        } else {
-            eprintln!("Invalid theme.");
-            theme::DEFAULT
-        }
-    } else { theme::DEFAULT };
+	let theme = if let Some(file) = matches.value_of("theme") {
+		if let Ok(theme) = theme::load(file) {
+			theme
+		} else {
+			eprintln!("Invalid theme.");
+			theme::DEFAULT
+		}
+	} else { theme::DEFAULT };
 
-    #[cfg(feature = "flame")]
-    flame::end("parse theme");
+	#[cfg(feature = "flame")]
+	flame::end("parse theme");
 
-    #[cfg(feature = "flame")]
-    flame::start("parse modules");
+	#[cfg(feature = "flame")]
+	flame::start("parse modules");
 
-    let modules: Vec<_> = matches
-        .values_of("modules")
-        .unwrap()
-        .map(|module| module.parse().unwrap())
-        .collect();
+	let modules: Vec<_> = matches
+		.values_of("modules")
+		.unwrap()
+		.map(|module| module.parse().unwrap())
+		.collect();
 
-    let time_format = matches.value_of("time_format").unwrap();
+	let time_format = matches.value_of("time_format").unwrap();
 
-    #[cfg(feature = "flame")]
-    flame::end("parse modules");
+	#[cfg(feature = "flame")]
+	flame::end("parse modules");
 
-    #[cfg(feature = "flame")]
-    flame::end("parse arguments");
+	#[cfg(feature = "flame")]
+	flame::end("parse arguments");
 
-    #[cfg(feature = "flame")]
-    flame::start("main");
+	#[cfg(feature = "flame")]
+	flame::start("main");
 
-    let mut p = Powerline {
-        segments: Vec::with_capacity(16), // just a guess
-        theme,
-        shell: match matches.value_of("shell").unwrap() {
-            "bare" => Shell::Bare,
-            "bash" => Shell::Bash,
-            "zsh"  => Shell::Zsh,
-            _ => unreachable!()
-        },
+	let mut p = Powerline {
+		segments: Vec::with_capacity(16), // just a guess
+		theme,
+		shell: match matches.value_of("shell").unwrap() {
+			"bare" => Shell::Bare,
+			"bash" => Shell::Bash,
+			"zsh"  => Shell::Zsh,
+			_ => unreachable!()
+		},
 
-        #[cfg(feature = "git2")]
-        git: None,
-        #[cfg(feature = "git2")]
-        git_statuses: None
-    };
+		#[cfg(feature = "git2")]
+		git: None,
+		#[cfg(feature = "git2")]
+		git_statuses: None
+	};
 
-    for module in modules {
-        match module {
-            Module::Cwd => segments::segment_cwd(&mut p, cwd_max_depth, cwd_max_dir_size),
-            Module::Git => { #[cfg(feature = "git2")] segments::segment_git(&mut p) },
-            Module::GitStage => { #[cfg(feature = "git2")] segments::segment_gitstage(&mut p) },
-            Module::Host => segments::segment_host(&mut p),
-            Module::Jobs => segments::segment_jobs(&mut p),
-            Module::NixShell => segments::segment_nix(&mut p),
-            Module::Perms => segments::segment_perms(&mut p),
-            Module::Ps => segments::segment_ps(&mut p),
-            Module::Root => segments::segment_root(&mut p, error),
-            Module::Ssh => segments::segment_ssh(&mut p),
-            Module::Time => segments::segment_time(&mut p, &time_format),
-            Module::User => segments::segment_user(&mut p),
-            Module::LineBreak => segments::segment_linebreak(&mut p),
-            Module::VirtualEnv => segments::segment_virtualenv(&mut p),
-        }
-    }
+	for module in modules {
+		match module {
+			Module::Cwd => segments::segment_cwd(&mut p, cwd_max_depth, cwd_max_dir_size),
+			Module::Git => { #[cfg(feature = "git2")] segments::segment_git(&mut p, git_max_size) },
+			Module::GitStage => { #[cfg(feature = "git2")] segments::segment_gitstage(&mut p) },
+			Module::Host => segments::segment_host(&mut p),
+			Module::Jobs => segments::segment_jobs(&mut p),
+			Module::NixShell => segments::segment_nix(&mut p),
+			Module::Perms => segments::segment_perms(&mut p),
+			Module::Ps => segments::segment_ps(&mut p),
+			Module::Root => segments::segment_root(&mut p, error),
+			Module::Ssh => segments::segment_ssh(&mut p),
+			Module::Time => segments::segment_time(&mut p, &time_format),
+			Module::User => segments::segment_user(&mut p),
+			Module::LineBreak => segments::segment_linebreak(&mut p),
+			Module::VirtualEnv => segments::segment_virtualenv(&mut p),
+		}
+	}
 
-    #[cfg(feature = "flame")]
-    flame::end("main");
-    #[cfg(feature = "flame")]
-    flame::start("print");
+	#[cfg(feature = "flame")]
+	flame::end("main");
+	#[cfg(feature = "flame")]
+	flame::start("print");
 
-    if matches.is_present("rtl") {
-        let n = p.segments.len();
-        for i in 1..n+1 {
-            p.segments[n-i].escape(p.shell);
-            p.segments[n-i].print_rtl(p.segments.get(n-i+1), p.shell, &p.theme);
-        }
-    } else {
-        for i in 0..p.segments.len() {
-            p.segments[i].escape(p.shell);
-            p.segments[i].print(p.segments.get(i+1), p.shell, &p.theme);
-        }
-    }
+	if matches.is_present("rtl") {
+		let n = p.segments.len();
+		for i in 1..n+1 {
+			p.segments[n-i].escape(p.shell);
+			p.segments[n-i].print_rtl(p.segments.get(n-i+1), p.shell, &p.theme);
+		}
+	} else {
+		for i in 0..p.segments.len() {
+			p.segments[i].escape(p.shell);
+			p.segments[i].print(p.segments.get(i+1), p.shell, &p.theme);
+		}
+	}
 
-    if matches.is_present("newline") {
-        println!();
-    } else if !matches.is_present("rtl") {
-        print!(" ");
-    }
+	if matches.is_present("newline") {
+		println!();
+	} else if !matches.is_present("rtl") {
+		print!(" ");
+	}
 
-    #[cfg(feature = "flame")]
-    flame::end("print");
+	#[cfg(feature = "flame")]
+	flame::end("print");
 
-    #[cfg(feature = "flame")]
-    {
-        use std::fs::File;
-        flame::dump_html(&mut File::create("profile.html").unwrap()).unwrap();
-    }
+	#[cfg(feature = "flame")]
+	{
+		use std::fs::File;
+		flame::dump_html(&mut File::create("profile.html").unwrap()).unwrap();
+	}
 }
--- src/segments/segment_git.rs
+++ src/segments/segment_git.rs
@@ -3,191 +3,200 @@ use crate::{Powerline, Segment};
 use git2::{BranchType, ObjectType, Repository, Status, StatusOptions, StatusShow};
 
 fn discover_if_none(git: &mut Option<Repository>) -> bool {
-    #[cfg(feature = "flame")]
-    let _guard = flame::start_guard("git discover");
+	#[cfg(feature = "flame")]
+	let _guard = flame::start_guard("git discover");
 
-    if git.is_none() {
-        *git = Repository::discover(".").ok();
-        git.is_some()
-    } else { true }
+	if git.is_none() {
+		*git = Repository::discover(".").ok();
+		git.is_some()
+	} else { true }
 }
 fn statuses_if_none(git: &Repository, statuses: &mut Option<Vec<Status>>) -> bool {
-    #[cfg(feature = "flame")]
-    let _guard = flame::start_guard("git status");
+	#[cfg(feature = "flame")]
+	let _guard = flame::start_guard("git status");
 
-    if statuses.is_none() {
-        *statuses = git.statuses(Some(
-                StatusOptions::new()
-                    .show(StatusShow::IndexAndWorkdir)
-                    .include_untracked(true)
-                    .renames_from_rewrites(true)
-                    .renames_head_to_index(true)
-            ))
-            .ok()
-            .map(|statuses|
-                statuses.iter()
-                .map(|entry| entry.status())
-                .collect());
-        statuses.is_some()
-    } else { true }
+	if statuses.is_none() {
+		*statuses = git.statuses(Some(
+				StatusOptions::new()
+					.show(StatusShow::IndexAndWorkdir)
+					.include_untracked(true)
+					.renames_from_rewrites(true)
+					.renames_head_to_index(true)
+			))
+			.ok()
+			.map(|statuses|
+				statuses.iter()
+				.map(|entry| entry.status())
+				.collect());
+		statuses.is_some()
+	} else { true }
 }
 
-pub fn segment_git(p: &mut Powerline) {
-    #[cfg(feature = "flame")]
-    let _guard = flame::start_guard("segment git");
+pub fn segment_git(p: &mut Powerline, git_max_size: u8) {
+	#[cfg(feature = "flame")]
+	let _guard = flame::start_guard("segment git");
 
-    if !discover_if_none(&mut p.git) {
-        return;
-    }
-    let git = p.git.as_ref().unwrap();
+	if !discover_if_none(&mut p.git) {
+		return;
+	}
+	let git = p.git.as_ref().unwrap();
 
-    #[cfg(feature = "flame")]
-    flame::start("iter branches");
+	#[cfg(feature = "flame")]
+	flame::start("iter branches");
+	let branches = git.branches(Some(BranchType::Local));
+	if branches.is_err() {
+		return;
+	}
 
-    let branches = git.branches(Some(BranchType::Local));
-    if branches.is_err() {
-        return;
-    }
+	let mut branch_name = None;
+	let mut local    = None;
+	let mut upstream = None;
 
-    let mut branch_name = None;
-    let mut local    = None;
-    let mut upstream = None;
+	for branch in branches.unwrap() {
+		if let Ok((branch, _)) = branch {
+			if branch.is_head() {
+				local    = branch.get().target();
+				upstream = branch.upstream().ok().and_then(|b| b.get().target());
 
-    for branch in branches.unwrap() {
-        if let Ok((branch, _)) = branch {
-            if branch.is_head() {
-                local    = branch.get().target();
-                upstream = branch.upstream().ok().and_then(|b| b.get().target());
+				if let Ok(name) = branch.name() {
+					if let Some(name) = name {
+						branch_name = Some(name.to_string());
+						break;
+					}
+				}
+			}
+		}
+	}
 
-                if let Ok(name) = branch.name() {
-                    if let Some(name) = name {
-                        branch_name = Some(name.to_string());
-                        break;
-                    }
-                }
-            }
+	#[cfg(feature = "flame")]
+	flame::end("iter branches");
+
+	if branch_name.is_none() {
+		#[cfg(feature = "flame")]
+		let _guard = flame::start_guard("search head");
+
+		// Could be a detached head
+		if let Ok(head) = git.head() {
+			if let Some(target) = head.target() {
+				branch_name = git.find_object(target, Some(ObjectType::Any))
+							.ok()
+							.and_then(|obj| obj.short_id().ok())
+							.and_then(|buf| buf.as_str()
+												.map(|s| s.to_string()))
+			}
+		} else {
+			p.segments.push(Segment::new(p.theme.git_dirty_bg, p.theme.git_dirty_fg, "Big Bang"));
+			return;
+		}
+	}
+
+	if !statuses_if_none(git, &mut p.git_statuses) {
+		return;
+	}
+	let statuses = p.git_statuses.as_ref().unwrap();
+
+	let (mut bg, mut fg) = (p.theme.git_dirty_bg, p.theme.git_dirty_fg);
+	if statuses.is_empty() {
+		bg = p.theme.git_clean_bg;
+		fg = p.theme.git_clean_fg;
+	}
+	let git_max_size = git_max_size as usize;
+	let mut branch_string = branch_name.unwrap();
+	if git_max_size > 0 && branch_string.chars().count() > git_max_size {
+        let mut start = 0;
+        for c in branch_string.chars().take(git_max_size) {
+            start += c.len_utf8();
         }
+        branch_string.drain(start..);
+        branch_string.push('…');
     }
+	p.segments.push(Segment::new(bg, fg, branch_string));
 
-    #[cfg(feature = "flame")]
-    flame::end("iter branches");
+	#[cfg(feature = "flame")]
+	let _guard = flame::start_guard("checking remotes");
 
-    if branch_name.is_none() {
-        #[cfg(feature = "flame")]
-        let _guard = flame::start_guard("search head");
+	if let Some(local) = local {
+		if let Some(upstream) = upstream {
+			if let Ok((ahead, behind)) = git.graph_ahead_behind(local, upstream) {
+				if ahead > 0 {
+					let mut ahead = if ahead == 1 { String::new() } else { ahead.to_string() };
+					ahead.push(p.theme.git_ahead_char);
+					p.segments.push(Segment::new(p.theme.git_ahead_bg, p.theme.git_ahead_fg, ahead));
+				}
 
-        // Could be a detached head
-        if let Ok(head) = git.head() {
-            if let Some(target) = head.target() {
-                branch_name = git.find_object(target, Some(ObjectType::Any))
-                            .ok()
-                            .and_then(|obj| obj.short_id().ok())
-                            .and_then(|buf| buf.as_str()
-                                                .map(|s| s.to_string()))
-            }
-        } else {
-            p.segments.push(Segment::new(p.theme.git_dirty_bg, p.theme.git_dirty_fg, "Big Bang"));
-            return;
-        }
-    }
-
-    if !statuses_if_none(git, &mut p.git_statuses) {
-        return;
-    }
-    let statuses = p.git_statuses.as_ref().unwrap();
-
-    let (mut bg, mut fg) = (p.theme.git_dirty_bg, p.theme.git_dirty_fg);
-    if statuses.is_empty() {
-        bg = p.theme.git_clean_bg;
-        fg = p.theme.git_clean_fg;
-    }
-    p.segments.push(Segment::new(bg, fg, branch_name.unwrap()));
-
-    #[cfg(feature = "flame")]
-    let _guard = flame::start_guard("checking remotes");
-
-    if let Some(local) = local {
-        if let Some(upstream) = upstream {
-            if let Ok((ahead, behind)) = git.graph_ahead_behind(local, upstream) {
-                if ahead > 0 {
-                    let mut ahead = if ahead == 1 { String::new() } else { ahead.to_string() };
-                    ahead.push(p.theme.git_ahead_char);
-                    p.segments.push(Segment::new(p.theme.git_ahead_bg, p.theme.git_ahead_fg, ahead));
-                }
-
-                if behind > 0 {
-                    let mut behind = if behind == 1 { String::new() } else { behind.to_string() };
-                    behind.push(p.theme.git_behind_char);
-                    p.segments.push(Segment::new(p.theme.git_behind_bg, p.theme.git_behind_fg, behind));
-                }
-            }
-        }
-    }
+				if behind > 0 {
+					let mut behind = if behind == 1 { String::new() } else { behind.to_string() };
+					behind.push(p.theme.git_behind_char);
+					p.segments.push(Segment::new(p.theme.git_behind_bg, p.theme.git_behind_fg, behind));
+				}
+			}
+		}
+	}
 }
 pub fn segment_gitstage(p: &mut Powerline) {
-    #[cfg(feature = "flame")]
-    let _guard = flame::start_guard("segment gitstage");
+	#[cfg(feature = "flame")]
+	let _guard = flame::start_guard("segment gitstage");
 
-    if !discover_if_none(&mut p.git) {
-        return;
-    }
-    let git = p.git.as_ref().unwrap();
+	if !discover_if_none(&mut p.git) {
+		return;
+	}
+	let git = p.git.as_ref().unwrap();
 
-    if !statuses_if_none(git, &mut p.git_statuses) {
-        return;
-    }
-    let statuses = p.git_statuses.as_ref().unwrap();
+	if !statuses_if_none(git, &mut p.git_statuses) {
+		return;
+	}
+	let statuses = p.git_statuses.as_ref().unwrap();
 
-    #[cfg(feature = "flame")]
-    flame::start("counting");
+	#[cfg(feature = "flame")]
+	flame::start("counting");
 
-    let mut staged = 0;
-    let mut notstaged = 0;
-    let mut untracked = 0;
-    let mut conflicted = 0;
+	let mut staged = 0;
+	let mut notstaged = 0;
+	let mut untracked = 0;
+	let mut conflicted = 0;
 
-    for status in statuses {
-        if status.contains(Status::INDEX_NEW)
-            || status.contains(Status::INDEX_MODIFIED)
-            || status.contains(Status::INDEX_TYPECHANGE)
-            || status.contains(Status::INDEX_RENAMED)
-            || status.contains(Status::INDEX_DELETED) {
-            staged += 1;
-        }
-        if status.contains(Status::WT_MODIFIED)
-            || status.contains(Status::WT_TYPECHANGE)
-            || status.contains(Status::WT_DELETED) {
-            notstaged += 1;
-        }
-        if status.contains(Status::WT_NEW) {
-            untracked += 1;
-        }
-        if status.contains(Status::CONFLICTED) {
-            conflicted += 1;
-        }
-    }
+	for status in statuses {
+		if status.contains(Status::INDEX_NEW)
+			|| status.contains(Status::INDEX_MODIFIED)
+			|| status.contains(Status::INDEX_TYPECHANGE)
+			|| status.contains(Status::INDEX_RENAMED)
+			|| status.contains(Status::INDEX_DELETED) {
+			staged += 1;
+		}
+		if status.contains(Status::WT_MODIFIED)
+			|| status.contains(Status::WT_TYPECHANGE)
+			|| status.contains(Status::WT_DELETED) {
+			notstaged += 1;
+		}
+		if status.contains(Status::WT_NEW) {
+			untracked += 1;
+		}
+		if status.contains(Status::CONFLICTED) {
+			conflicted += 1;
+		}
+	}
 
-    #[cfg(feature = "flame")]
-    flame::end("counting");
+	#[cfg(feature = "flame")]
+	flame::end("counting");
 
-    if staged > 0 {
-        let mut string = if staged == 1 { String::with_capacity(1) } else { staged.to_string() };
-        string.push(p.theme.git_staged_char);
-        p.segments.push(Segment::new(p.theme.git_staged_bg, p.theme.git_staged_fg, string));
-    }
-    if notstaged > 0 {
-        let mut string = if notstaged == 1 { String::with_capacity(1) } else { notstaged.to_string() };
-        string.push(p.theme.git_notstaged_char);
-        p.segments.push(Segment::new(p.theme.git_notstaged_bg, p.theme.git_notstaged_fg, string));
-    }
-    if untracked > 0 {
-        let mut string = if untracked == 1 { String::with_capacity(1) } else { untracked.to_string() };
-        string.push(p.theme.git_untracked_char);
-        p.segments.push(Segment::new(p.theme.git_untracked_bg, p.theme.git_untracked_fg, string));
-    }
-    if conflicted > 0 {
-        let mut string = if conflicted == 1 { String::with_capacity(1) } else { conflicted.to_string() };
-        string.push(p.theme.git_conflicted_char);
-        p.segments.push(Segment::new(p.theme.git_conflicted_bg, p.theme.git_conflicted_fg, string));
-    }
+	if staged > 0 {
+		let mut string = if staged == 1 { String::with_capacity(1) } else { staged.to_string() };
+		string.push(p.theme.git_staged_char);
+		p.segments.push(Segment::new(p.theme.git_staged_bg, p.theme.git_staged_fg, string));
+	}
+	if notstaged > 0 {
+		let mut string = if notstaged == 1 { String::with_capacity(1) } else { notstaged.to_string() };
+		string.push(p.theme.git_notstaged_char);
+		p.segments.push(Segment::new(p.theme.git_notstaged_bg, p.theme.git_notstaged_fg, string));
+	}
+	if untracked > 0 {
+		let mut string = if untracked == 1 { String::with_capacity(1) } else { untracked.to_string() };
+		string.push(p.theme.git_untracked_char);
+		p.segments.push(Segment::new(p.theme.git_untracked_bg, p.theme.git_untracked_fg, string));
+	}
+	if conflicted > 0 {
+		let mut string = if conflicted == 1 { String::with_capacity(1) } else { conflicted.to_string() };
+		string.push(p.theme.git_conflicted_char);
+		p.segments.push(Segment::new(p.theme.git_conflicted_bg, p.theme.git_conflicted_fg, string));
+	}
 }
